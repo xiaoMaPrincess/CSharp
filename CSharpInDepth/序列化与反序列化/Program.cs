55d21d7464f07faaf78e835a7b40996f4f066add
xiaoMaPrincess <xiaoMaPrincess@gmail.com>
2019-05-25 10:40:38 +0800

    专用线程的简单调用

---
+++ CSharpInDepth/序列化与反序列化/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace 序列化与反序列化
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "Jee", "Allen" };
+            Stream stream = SerializeToMemory(list);
+            Console.WriteLine(stream.ToString());
+            stream.Position = 0;
+            list = null;
+            list = (List<string>)DeSerializeFromMemory(stream);
+            // 测试是否反序列化成功
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        static MemoryStream SerializeToMemory(object obj)
+        {
+            // 构造流容纳序列化对象
+            MemoryStream stream = new MemoryStream();
+            // 构造序列化格式化器实例
+            BinaryFormatter formatter = new BinaryFormatter();
+            // 将对象序列化到流中
+            formatter.Serialize(stream, obj);
+            return stream;
+        }
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        static object DeSerializeFromMemory(Stream stream)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            return formatter.Deserialize(stream);
+        }
+    }
+}
---
+++ CSharpInDepth/线程/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace 线程
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("线程的调用：");
+            Thread thread = new Thread(Add);
+            thread.Start(5);
+            Thread.Sleep(1000);
+            Console.WriteLine("主线程做其他的工作.......");
+            thread.Join();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 线程要调用的方法
+        /// </summary>
+        /// <param name="state"></param>
+        private static void Add(object state)
+        {
+            Console.WriteLine($"开始调用另一个线程:{state}");
+            // 模拟做其他任务所用的时间  ，这个方法返回后，专用线程将停止调用
+            Thread.Sleep(10000);
+        }
+    }
+}
