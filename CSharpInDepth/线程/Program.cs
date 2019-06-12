5b0cbb931d59c2c8225e0a55de26a001bd6740ef
xiaoMaPrincess <xiaoMaPrincess@gmail.com>
2019-06-12 19:56:42 +0800

    任务

--- CSharpInDepth/线程/Program.cs
+++ CSharpInDepth/线程/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace 线程
 {
@@ -7,12 +8,38 @@ namespace 线程
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("线程的调用：");
-            Thread thread = new Thread(Add);
-            thread.Start(5);
-            Thread.Sleep(1000);
-            Console.WriteLine("主线程做其他的工作.......");
-            thread.Join();
+
+
+            Console.WriteLine("主线程启动.....");
+            //Task<int> t = Task.Run(() =>Sum(100));
+            //t.ContinueWith(task => Console.WriteLine($"sum: {task.Result}"),TaskContinuationOptions.OnlyOnCanceled);
+            //t.ContinueWith(task => Console.WriteLine($"sum: {task.Result}"));
+            Status();
+           // Thread.Sleep(10000);// 模拟任务10秒
+            Console.WriteLine("主线程继续工作.....");
+
+
+
+
+            #region 线程池异步调用
+            //Console.WriteLine("主线程.........");
+            //ThreadPool.QueueUserWorkItem(ComputeBoundOp,5);
+            //Console.WriteLine("主线程做其他任务.........");
+            //Thread.Sleep(10000);// 模拟任务10秒
+            //Console.WriteLine("主线程任务完成.........");
+            #endregion
+
+            #region 线程调用初级版
+
+            //Console.WriteLine("线程的调用：");
+            //Thread thread = new Thread(Add);
+            //thread.Start(5);
+            //Thread.Sleep(1000);
+            //Console.WriteLine("主线程做其他的工作.......");
+            //thread.Join();
+            #endregion
+
+
             Console.ReadKey();
         }
 
@@ -25,6 +52,40 @@ namespace 线程
             Console.WriteLine($"开始调用另一个线程:{state}");
             // 模拟做其他任务所用的时间  ，这个方法返回后，专用线程将停止调用
             Thread.Sleep(10000);
+        }
+
+        /// <summary>
+        /// 异步调用的方法：必须匹配WaiCallback委托
+        /// </summary>
+        /// <param name="state"></param>
+        private static void ComputeBoundOp(object state)
+        {
+            Console.WriteLine($"辅助线程{state}");
+            Thread.Sleep(1000);
+        }
+
+        /// <summary>
+        /// Task调用
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static int Sum(int n)
+        {
+            int sum = 0;
+            for (;  n>0; n--)
+            {
+                sum += n;
+            }
+            return sum;
+        }
+
+        private static async void Status()
+        {
+           await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("异步调用");
+            });
         }
     }
 }
